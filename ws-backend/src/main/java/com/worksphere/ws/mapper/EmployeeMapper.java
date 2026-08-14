2c141326e5225e797f78db86f094e5b1f16488da
Amol Shinde <shindeamol409@gmail.com>
2026-08-15 04:39:35 +0530

    Add employee controller with post method

--- ws-backend/src/main/java/com/worksphere/ws/mapper/EmployeeMapper.java
+++ ws-backend/src/main/java/com/worksphere/ws/mapper/EmployeeMapper.java
@@ -19,7 +19,7 @@ public class EmployeeMapper {
                 employeeDto.getId(),
                 employeeDto.getFirstName(),
                 employeeDto.getLastName(),
-                employeeDto.getLastName()
+                employeeDto.getEmail()
         );
     }
 }
