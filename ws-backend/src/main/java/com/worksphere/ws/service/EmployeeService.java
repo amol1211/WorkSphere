88089a44ce29053049038e9d69d5faa2c2f94c21
Amol Shinde <shindeamol409@gmail.com>
2026-08-16 03:39:31 +0530

    Add get all employees rest api

--- ws-backend/src/main/java/com/worksphere/ws/service/EmployeeService.java
+++ ws-backend/src/main/java/com/worksphere/ws/service/EmployeeService.java
@@ -2,8 +2,12 @@ package com.worksphere.ws.service;
 
 import com.worksphere.ws.dto.EmployeeDto;
 
+import java.util.List;
+
 public interface EmployeeService {
     EmployeeDto createEmployee(EmployeeDto employeeDto);
 
     EmployeeDto getEmployeeById(Long employeeId);
+
+    List<EmployeeDto> getAllEmployees();
 }
--- ws-backend/src/main/java/com/worksphere/ws/service/impl/EmployeeServiceImpl.java
+++ ws-backend/src/main/java/com/worksphere/ws/service/impl/EmployeeServiceImpl.java
@@ -9,6 +9,9 @@ import com.worksphere.ws.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
@@ -32,4 +35,13 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         return EmployeeMapper.mapToEmployeeDto(employee);
     }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
+        return employees.stream().map((employee) -> EmployeeMapper.mapToEmployeeDto(employee))
+                .collect(Collectors.toList());
+    }
+
+
 }
