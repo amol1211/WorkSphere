53c4c26e3d0a77be4f9dbe61e6057368c84ab595
Amol Shinde <shindeamol409@gmail.com>
2026-08-17 20:33:36 +0530

    Create  add employee component

--- ws-frontend/src/App.jsx
+++ ws-frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import EmployeeComponent from "./components/EmployeeComponent";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<ListEmployeeComponent />}></Route>
           <Route path="/employees" element={<ListEmployeeComponent />}></Route>
+          <Route path="/add-employee" element={<EmployeeComponent />}></Route>
         </Routes>
         <Footer />
       </BrowserRouter>
