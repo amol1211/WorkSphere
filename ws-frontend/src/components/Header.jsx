8d783402991acc97e1b7813a9cd6ba31ad604d55
Amol Shinde <shindeamol409@gmail.com>
2026-08-17 19:56:11 +0530

    Add header and footer component

---
+++ ws-frontend/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <header>
+        <nav className="navbar navbar-dark bg-primary">
+          <a className="navbar-brand" href="#">
+            WorkSphere
+          </a>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
